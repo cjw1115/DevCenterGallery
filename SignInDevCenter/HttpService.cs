0cf1dc533791ec5b833677f46ca21a314ac7936e
陈佳卫 <cjw1115@hotmail.com>
2020-04-16 02:28:49 +0800

    Add project files.

---
+++ SignInDevCenter/HttpService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SignInDevCenter
+{
+    public class HttpService
+    {
+        HttpClientHandler _handler = null;
+        HttpClient _client = null;
+        public HttpService()
+        {
+            _handler = new HttpClientHandler();
+            _handler.AllowAutoRedirect = false;
+            _client = new HttpClient(_handler);
+        }
+
+        public async Task<HttpResponseMessage> SendRequest(string url, HttpContent content = null, bool isPost = false)
+        {
+            HttpRequestMessage request = null;
+            try
+            {
+                request = new HttpRequestMessage(isPost ? HttpMethod.Post : HttpMethod.Get, url);
+                if (isPost)
+                {
+                    request.Content = content;
+                }
+                return await _client.SendAsync(request);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                request?.Dispose();
+            }
+        }
+    }
+}
---
+++ SignInDevCenter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SignInDevCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Microsoft!");
+
+            SignInDevCenter demo = new SignInDevCenter();
+            demo.Run().Wait();
+        }
+    }
+}
